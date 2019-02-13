2576ea94858b50e1e768679faed43d32e7a269fa
Kennith Mann <kmann@etherpunk.com>
2019-02-13 16:12:22 -0600

    Adding layouts based on roles and tweaking login.

---
+++ EtherpunkInventoryManagement/Views/Shared/_Layout_Superviser.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtherpunkInventoryManagement.Views.Shared
+{
+    public class _Layout_SuperviserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
